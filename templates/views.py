a697f4b3efe9128cb1e5b6e472890d92f15601e1
Dan <dansonlinepresence@gmail.com>
2023-02-25 12:53:12 -0500

    import require_http_methods in views.py

--- templates/views.py
+++ templates/views.py
@@ -3,6 +3,7 @@ from . import models
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
 
 import json
 
